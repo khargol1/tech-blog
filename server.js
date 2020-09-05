7dc837d46024bef6a695579328a197c0845944e5
Khargol <josephklein3@gmail.com>
2020-09-05 10:01:54 -0400

    date formatting, minor html rework

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helper');
 const app = express();
 const PORT = process.env.PORT || 3001;
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 
 require('dotenv').config();
 
